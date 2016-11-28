2454d6b03fc2b938b8ce711dd6ae39356ce396a3
Jordan Deang <jdeang@vt.edu>
2016-11-27 22:34:43 -0500

    account creation/login working

--- src/main/java/com/WebSecurityConfig.java
+++ src/main/java/com/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/createAccount", "/refreshStocks").permitAll()
+                .antMatchers("/", "/home", "/signup", "/createAccount", "/refreshStocks").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
