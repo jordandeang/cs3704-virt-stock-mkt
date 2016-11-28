d0ad60539f398548e1245539af32683fe7023c2a
Kevin Koehncke <koehncke>
2016-11-28 11:09:55 -0500

    Created integration test for selling stock

--- src/test/java/com/controller/RestApiControllerTest.java
+++ src/test/java/com/controller/RestApiControllerTest.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.model.Stock;
 import com.model.User;
 import com.repository.userRepository;
 import org.junit.Test;
@@ -88,7 +89,7 @@ public class RestApiControllerTest {
         assertEquals(response.getStatusCode(), HttpStatus.OK);
         Map createdUser = response.getBody();
         User userUpdated = userRepository.findByName(createdUser.get("name").toString());
-        assertEquals(userUpdated.getBalance(), user.getBalance() - userUpdated.getStocklist().get(0).getStockPrice()*userUpdated.getStocklist().get(0).getNumberOfShares(), 1);
+        assertEquals(userUpdated.getBalance(), user.getBalance() - userUpdated.getStocklist().get(0).getStockPrice() * userUpdated.getStocklist().get(0).getNumberOfShares(), 1);
         assertEquals(userUpdated.getStocklist().get(0).getStockName(), "Apple");
         assertEquals(userUpdated.getStocklist().get(0).getStockSymbol(), "AAPL");
         assertEquals(userUpdated.getStocklist().get(0).getNumberOfShares(), 10);
@@ -97,7 +98,38 @@ public class RestApiControllerTest {
 
     @Test
     public void sellStock() throws Exception {
-
+        String baseURL = "http://localhost:8080/sellStock";
+        //Save initial user with initial stock
+        User user = new User("Kevin", "asdasd", "password", "kkoehncke@hotmail.com", 10000);
+        Stock stock = new Stock("Apple", "AAPL", 112, 10);
+        user.addStock(stock);
+        userRepository.save(user);
+        //Building the Request body data
+        Map<String, Object> requestBody = new HashMap<String, Object>();
+        requestBody.put("stockName", "Apple");
+        requestBody.put("stockSymbol", "AAPL");
+        requestBody.put("numberOfShares", 9);
+        requestBody.put("userName", "Kevin");
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+        //Creating http entity object with request body and headers
+        HttpEntity<String> httpEntity =
+                new HttpEntity<String>(OBJECT_MAPPER.writeValueAsString(requestBody), requestHeaders);
+        ResponseEntity<Map> response =
+                restTemplate.postForEntity(baseURL + "?stockName=" +
+                        requestBody.get("stockName") + "&stockSymbol="
+                        + requestBody.get("stockSymbol")
+                        + "&numberOfShares=" + requestBody.get("numberOfShares")
+                        + "&userName=" + requestBody.get("userName"), httpEntity, Map.class, Collections.EMPTY_MAP);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Map createdUser = response.getBody();
+        User userUpdated = userRepository.findByName(createdUser.get("name").toString());
+        int numberOfSharesSold = (int) requestBody.get("numberOfShares");
+        assertEquals(userUpdated.getBalance(), user.getBalance() + userUpdated.getStocklist().get(0).getStockPrice()*numberOfSharesSold, 1);
+        assertEquals(userUpdated.getStocklist().get(0).getStockName(), "Apple");
+        assertEquals(userUpdated.getStocklist().get(0).getStockSymbol(), "AAPL");
+        assertEquals(userUpdated.getStocklist().get(0).getNumberOfShares(), 1);
+        userRepository.deleteAll();
     }
-
 }
+
