cb7e2a8c0909e2c80e633a4bc1fa1999988182c8
mihirk93 <mihirk93@vt.edu>
2016-11-28 11:51:08 -0500

    Update

--- src/main/java/com/controller/MongoController.java
+++ src/main/java/com/controller/MongoController.java
@@ -1,12 +1,16 @@
 package com.controller;
 
 import com.model.User;
-import com.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.repository.userRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Kevin on 11/14/2016.
@@ -17,26 +21,39 @@ public class MongoController  implements UserDetailsService {
     @Autowired
     private userRepository userRepository;
 
+    /**
+    * Saves the new user account to the repository
+    */
     public void createAccount(User user) {
         userRepository.save(user);
     }
-
+    
+    /**
+    * Searches for and returns the requested user by their name
+    */
     public User getUser(String user) {
         return userRepository.findByName(user);
     }
 
+    /**
+    * Updates the user's information
+    */
     public User updateUser(User user) {
         userRepository.save(user);
         return user;
     }
 
+    /**
+    * Displays the user's details
+    */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByName(username);
         if(user == null){
             throw new UsernameNotFoundException(username);
         }else{
-            return user;
+            List<SimpleGrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("USER"));
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
         }
     }
 }
